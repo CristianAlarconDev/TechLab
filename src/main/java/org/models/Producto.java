6da42069246b620842a6e1e879408dbccf922f39
Cristian <calarcon@fi.uba.ar>
2026-05-10 22:01:35 -0300

    cambio de estructura

--- src/main/java/org/models/Producto.java
+++ src/main/java/org/models/Producto.java
@@ -1,15 +1,18 @@
-package models;
+package learning.models;
 
 public class Producto {
     private String nombre;
     private String descripcion;
     private int precio;
     private int ID;
-    public Producto(String nombre, String descripcion, int precio, int ID) {
+    private int stock;
+
+    public Producto(String nombre, String descripcion, int precio, int ID, int stock) {
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.precio = precio;
         this.ID = ID;
+        this.stock = stock;
     }
     public String obtenerNombre() {
         return nombre;
@@ -20,5 +23,8 @@ public class Producto {
     public int obtenerPrecio() {
         return precio;
     }
+    public int actualizarPrecio(int precio) {
 
+        return this.precio = precio;
+    }
 }
