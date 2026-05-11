104eb899f65ea6a78e5e55f3b14b8dcd3a184bef
Cristian <calarcon@fi.uba.ar>
2026-05-11 17:54:30 -0300

    prod service ahora devuelve un vendible dado el id validando el mismo y se maneja con una exception nueva al no encontrar uno valido

--- src/main/java/org/services/ProductoService.java
+++ src/main/java/org/services/ProductoService.java
@@ -1,4 +1,43 @@
 package org.services;
 
+import org.exceptions.CantidadInvalidaException;
+import org.exceptions.IdNoEncontradoException;
+import org.models.Producto;
+import org.models.Vendible;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductoService {
+    private List<Producto> productos;
+    public ProductoService()
+    {
+        productos = new ArrayList<Producto>();
+    }
+    public void agregarProducto(Producto producto)
+    {
+        productos.add(producto);
+    }
+    public void  eliminarProducto(Producto producto)
+    {
+        productos.remove(producto);
+    }
+    public Vendible obtenerVendible(int idProducto)
+    {
+        if(idProducto <= 0){
+            throw new CantidadInvalidaException("El id es invalido");
+        }
+
+        Vendible vendible = null;
+        for (Producto producto: productos)
+        {   /*delegar luego a producto la comparación*/
+            if (producto.obtenerID()==idProducto){
+                vendible=producto;
+            }
+        }
+        if (vendible==null){
+            throw new IdNoEncontradoException("No existe el producto con el id: "+idProducto);
+        }
+        return vendible;
+    }
 }
