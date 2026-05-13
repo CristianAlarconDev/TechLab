d041980861681e1c7568fef99be5cc8dac12f560
CristianAlarconDev <calarcon@fi.uba.ar>
2026-05-13 20:49:32 -0300

    generador ahora valida si el id que recibe por parametro es mayor al suyo, necesario para validar cuando no se sincronizo su estado

--- src/main/java/techlab/models/GeneradorID.java
+++ src/main/java/techlab/models/GeneradorID.java
@@ -12,4 +12,9 @@ public class GeneradorID {
     public boolean idExiste(int id){
         return (id<=this.id) && (id>0);
     }
+    public void sincronizarSiEsMayor(int idExistente) {
+        if (idExistente > this.id) {
+            this.id = idExistente;
+        }
+    }
 }
