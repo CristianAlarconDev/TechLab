df08329018542b16e3ca9192a92e3bc235090a83
Cristian <calarcon@fi.uba.ar>
2026-05-13 12:08:50 -0300

    cambio de nombre de paquete, org a techlab

---
+++ src/main/java/techlab/services/PedidoService.java
@@ -0,0 +1,73 @@
+package techlab.services;
+
+import techlab.exceptions.IdNoEncontradoException;
+import techlab.exceptions.PedidoNoIniciadoException;
+import techlab.models.GeneradorID;
+import techlab.models.Pedido;
+import techlab.models.Vendible;
+import techlab.models.ItemPedido;
+
+public class PedidoService {
+    private ProductoService productoService;
+    private GeneradorID generador;
+    private Pedido pedidoEnProceso;
+    public PedidoService( ProductoService productoService)
+    {
+        this.generador = new GeneradorID();
+        this.pedidoEnProceso = null;
+        this.productoService = productoService;
+    }
+    public void iniciarPedido()
+    {
+        pedidoEnProceso = new Pedido(generador.obtenerId());
+
+    }
+    public void agregarItemAlPedido(int idProducto, int cantidad){
+        checkPedidoEnProceso();
+        try {
+            if(productoService.hayStockDeProducto(idProducto, cantidad)){
+                Vendible vendible = productoService.obtenerVendible(idProducto);
+                ItemPedido nuevoItem = new ItemPedido(cantidad, vendible);
+                pedidoEnProceso.agregarItem(nuevoItem);
+                System.out.println("Agregado: " + vendible.obtenerNombre() + " x" + cantidad);
+            }
+
+        }
+        catch (Exception error)
+            {
+            System.err.println("Error al agregar el item al pedido." + error.getMessage());
+            }
+    }
+    public void eliminarItemAlPedido(int idVendible){
+        checkPedidoEnProceso();
+        try {
+            pedidoEnProceso.removerItem(idVendible);
+        } catch (IdNoEncontradoException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    public double obtenerTotal(){
+        checkPedidoEnProceso();
+        return pedidoEnProceso.obtenerTotal();
+    }
+    public void finalizarPedido(){
+        checkPedidoEnProceso();
+        try {
+            for (ItemPedido item : pedidoEnProceso.obtenerItems()) {
+                productoService.descontarStock(item.obtenerId(), item.obtenerCantidad());
+            }
+            System.out.println("Pedido finalizado con éxito.");
+            System.out.println(pedidoEnProceso.obtenerResumen());
+
+            pedidoEnProceso = null;
+        } catch (Exception e) {
+            System.err.println("Error crítico al procesar el pedido: " + e.getMessage());
+        }
+    }
+    private void checkPedidoEnProceso(){
+        if(pedidoEnProceso == null)
+        {   throw new PedidoNoIniciadoException("Pedido no iniciado.");
+        }
+    }
+
+}
---
+++ src/main/java/techlab/services/ProductoService.java
@@ -0,0 +1,70 @@
+package techlab.services;
+
+import techlab.exceptions.IdNoEncontradoException;
+import techlab.models.Producto;
+import techlab.models.Vendible;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductoService {
+    private List<Producto> productos;
+    public ProductoService()
+    {
+        productos = new ArrayList<Producto>();
+    }
+    public void agregarProducto(Producto producto)
+    {
+        productos.add(producto);
+    }
+    public void  eliminarProducto(int idProducto) {
+        /*luego llevar los print a una clase o delegarlo*/
+        try {
+            Producto producto = obtenerProducto(idProducto);
+            productos.remove(producto);
+            System.out.println("Éxito: El producto ha sido eliminado del sistema.");
+
+        } catch (IdNoEncontradoException error) {
+            System.out.println("Error: " + error.getMessage());
+        }
+    }
+    public Vendible obtenerVendible(int idProducto)
+    {   //recordar que quien use vendible debe usar try-catch para el manejo
+        return obtenerProducto(idProducto);
+    }
+    private Producto obtenerProducto(int idProducto)
+    {
+        if(idProducto <= 0){
+            throw new IdNoEncontradoException("El id es invalido");
+        }
+        for(Producto producto: productos){
+            if (producto.poseeElId(idProducto)){
+                return  producto;
+            }
+        }
+        throw new IdNoEncontradoException("No existe el producto con ID: " + idProducto);
+    }
+   public boolean hayStockDeProducto(int idProducto, int cantidad)
+    {
+        Producto producto = obtenerProducto(idProducto);
+        return producto.tieneStockPara(cantidad);
+    }
+    public void descontarStock(int idProducto, int cantidad)
+    {
+        Producto producto = obtenerProducto(idProducto);
+        producto.reducirStock(cantidad);
+    }
+    public void mostrarProductos() {
+        if (productos.isEmpty()) {
+            System.out.println("El inventario está vacío.");
+            return;
+        }
+        System.out.println("=============================================================================");
+        System.out.println("| ID    | Nombre     | Precio     | Descripción          | Stock |");
+        System.out.println("=============================================================================");
+        for (Producto producto : productos) {
+            System.out.println(producto.mostrarInformacion());
+        }
+        System.out.println("=============================================================================");
+    }
+}
