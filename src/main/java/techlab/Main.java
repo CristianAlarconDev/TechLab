75cbe71898e5ac04b1ce071559f969f7cc0915a4
Cristian <calarcon@fi.uba.ar>
2026-05-13 15:41:49 -0300

    version con funcionalidades completas del sistema, falta refactorizar service pedido , el main carga productos de un json y luego inicializa el menu

--- src/main/java/techlab/Main.java
+++ src/main/java/techlab/Main.java
@@ -1,18 +1,28 @@
 package techlab;
 
+import techlab.models.MenuPrincipal;
 import techlab.models.Parser;
 import techlab.models.Producto;
 import techlab.services.ProductoService;
 import techlab.services.PedidoService;
-import java.util.Scanner;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        ProductoService productoService = new ProductoService();
+        PedidoService pedidoService = new PedidoService(productoService);
+        Parser parser = new Parser();
+        try {
+            List<Producto> productosJson = parser.leerProductos("src/main/resources/productos.json");
+            for (Producto p : productosJson) {
+                productoService.agregarProducto(p);
+            }
+            System.out.println("Sistema iniciado: " + productosJson.size() + " productos cargados.");
+        } catch (Exception e) {
+            System.err.println("Aviso: No se detectó archivo inicial. Iniciando con inventario vacío.");
+        }
 
-
-        // --- Sub-flujos del menú ---
-
-
+        MenuPrincipal menu = new MenuPrincipal(productoService, pedidoService);
+        menu.lanzar();
     }
 }
