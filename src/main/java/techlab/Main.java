2b8ff9ea78fda44a89906a39478f0502eff6d966
Cristian <calarcon@fi.uba.ar>
2026-05-13 12:12:02 -0300

    main formateado para ir sumando las opciones menu

---
+++ src/main/java/techlab/Main.java
@@ -0,0 +1,18 @@
+package techlab;
+
+import techlab.models.Parser;
+import techlab.models.Producto;
+import techlab.services.ProductoService;
+import techlab.services.PedidoService;
+import java.util.Scanner;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        // --- Sub-flujos del menú ---
+
+
+    }
+}
